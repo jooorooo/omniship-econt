62943de5a4b871837466ec230f075ae2a7ddc8fb
Georgi Nachev <g.nachev@webmedia.bg>
2017-05-17 14:29:54 +0300

    init

---
+++ src/Gateway.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joro
+ * Date: 11.5.2017 г.
+ * Time: 17:18 ч.
+ */
+
+namespace Omniship\Speedy;
+
+use Omniship\Speedy\Http\ShippingServicesRequest;
+use Omniship\Speedy\Http\TrackingParcelRequest;
+use Omniship\Common\AbstractGateway;
+
+class Gateway extends AbstractGateway
+{
+
+    private $name = 'Speedy';
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param $name
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getDefaultParameters()
+    {
+        return array(
+            'username' => '',
+            'password' => '',
+        );
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->getParameter('username');
+    }
+
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function setUsername($value)
+    {
+        return $this->setParameter('username', $value);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->getParameter('password');
+    }
+
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function setPassword($value)
+    {
+        return $this->setParameter('password', $value);
+    }
+
+    /**
+     * @param array $parameters
+     * @return \Omniship\Message\AbstractRequest
+     */
+    public function getServices(array $parameters = [])
+    {
+        return $this->createRequest(ShippingServicesRequest::class, $this->getParameters() + $parameters);
+    }
+
+    /**
+     * @param array $parameters
+     * @return \Omniship\Message\AbstractRequest
+     */
+    public function trackingParcel(array $parameters = [])
+    {
+        return $this->createRequest(TrackingParcelRequest::class, $this->getParameters() + $parameters);
+    }
+    /**
+     * Supports Cash On Delivery
+     *
+     * @return boolean True if this gateway supports the Cash On Delivery
+     */
+    public function supportsCashOnDelivery()
+    {
+        return false;
+    }
+    /**
+     * Supports Insurance
+     *
+     * @return boolean True if this gateway supports the Insurance
+     */
+    public function supportsInsurance()
+    {
+        return true;
+    }
+}
